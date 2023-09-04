83b16497d3107fd98d78714ef03d57df8b903a4d
valentina.insuasty <valentina01010206@gmail.com>
2023-09-04 00:56:48 -0500

    Se arreglo NumeroPalindromo

--- Taller2_Programacion/NumeroPalindromo.java
+++ Taller2_Programacion/NumeroPalindromo.java
@@ -2,29 +2,28 @@ package Taller2_Programacion;
 import java.util.Scanner;
 public class NumeroPalindromo {
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingrese la cantidad de datos: ");
-            int cantidadDatos = scanner.nextInt();
-            if (cantidadDatos <= 0) {
-                System.out.println("La cantidad de datos debe ser mayor que cero.");
-                return;
-            }
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int num1 = sc.nextInt();
 
-            for (int i = 1; i <= cantidadDatos; i++) {
-                System.out.print("Ingrese el dato #" + i + ": ");
-                int dato = scanner.nextInt();
-                if (dato < min) {
-                    min = dato;
-                }
-
-                if (dato > max) {
-                    max = dato;
-                }
-            }
-
-            System.out.println("El rango de los datos ingresados es: " + (max - min));
+        if (Palindromo(num1)) {
+            System.out.println(num1 + " es un numero palindromo.");
+        } else {
+            System.out.println(num1 + " no es un numero palindromo.");
         }
 
     }
+
+    public static boolean Palindromo(int num) {
+        int numA = num;
+        int numR = 0;
+
+        while (num > 0) {
+            int dig = num % 10;
+            numR = numR * 10 + dig;
+            num /= 10;
+        }
+
+        return numA == numR;
+    }
+}
